c25fef5197556e04b26558348e102644d9759100
HoneyMS <Honeyms@mail.ru>
2021-02-24 11:22:19 +0300

    for heroku

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const cors = require('cors');
 const path = require('path');
 const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
-
-
+const dotenv = require('dotenv');
+dotenv.config()
 const app = express();
 app.use(express.static(path.resolve('client/build/')))
 
@@ -21,15 +21,15 @@ app.get('/downloadFile',async (req, res)=>{
 })
 
 
-// const root = path.join(process.env.PWD, 'client', 'build');
-// app.use(express.static(root));
+const root = path.join(process.env.PWD, 'client', 'build');
+app.use(express.static(root));
 
-// app.get('*', (req, res) => {
-//   res.sendFile('index.html', { root });
-// });
 app.get('*', (req, res) => {
-	   res.sendFile(path.resolve('client/build/index.html'))
-	 })
+  res.sendFile('index.html', { root });
+});
+// app.get('*', (req, res) => {
+// 	   res.sendFile(path.resolve('client/build/index.html'))
+// 	 })
 const PORT = 8080
 app.listen(PORT, console.log(`server is work ${PORT}`));
 
